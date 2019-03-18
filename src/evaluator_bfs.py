9ab6abe7d0ece84377b294d9dd9ce66188d322b6
Federico Lochbaum <fede@SCVsofts-MacBook-Pro-2.local>
2019-03-17 21:21:03 -0300

    refactoring code of bfs evaluator, continue using dfs evaluation

--- src/evaluator_bfs.py
+++ src/evaluator_bfs.py
@@ -109,24 +109,22 @@ class Evaluator:
     def eval_integer_constant(self, expr):
         yield values.IntegerConstant(expr.value)
 
-#######
     def eval_variable_or_constructor(self, expr, env):
         if env.is_defined(expr.name):
-            value0 = env.value(expr.name)
-            for value in self.eval_value(value0):
-                env.set(expr.name, value)
-                yield value
-                env.set(expr.name, value0)
-        elif expr.name in self._constructors:
-            yield values.RigidStructure(expr.name, [])
-        elif expr.name in self._primitives:
-            yield values.Primitive(expr.name, [])
-        else:
-            raise Exception(
-                    'Name {name} is not a variable nor a constructor.'.format(
-                      name=expr.name
-                    )
-                  )
+            yield from self.eval_defined_variable_or_constructor(expr, env)
+        else: 
+            yield (
+                values.RigidStructure(expr.name, []) if expr.name in self._constructors
+                else values.Primitive(expr.name, []) if expr.name in self._primitives
+                else exception_with('Name {name} is not a variable nor a constructor.'.format(name=expr.name))
+            )
+
+    def eval_defined_variable_or_constructor(self, expr, env):
+        value0 = env.value(expr.name)
+        for value in self.eval_value(value0):
+            env.set(expr.name, value)
+            yield value
+            env.set(expr.name, value0)
 
     def eval_lambda(self, expr, env):
         yield values.Closure(expr.var, expr.body, env)
@@ -137,12 +135,10 @@ class Evaluator:
 
     def eval_let(self, expr, env):
         extended_env = env.extended()
-        exprs = []
         for decl in expr.declarations:
-            if not decl.is_definition():
-                continue
-            extended_env.define(decl.lhs.name,
-                                values.Thunk(decl.rhs, extended_env))
+            if not decl.is_definition(): continue
+            extended_env.define(decl.lhs.name, values.Thunk(decl.rhs, extended_env))
+
         yield from self.eval_expression(expr.body, extended_env)
 
     def eval_fresh(self, expr, env):
@@ -152,47 +148,40 @@ class Evaluator:
         yield from self.eval_expression(expr.body, extended_env)
 
     def eval_value(self, value):
-        if value.is_decided():
-            yield value
-        elif value.is_thunk():
-            yield from self.eval_expression(value.expr, value.env)
-        elif value.is_flex_structure():
-            assert value.symbol.is_instantiated() # undecided
-            yield from self.apply_many(
-                         value.symbol.representative(),
-                         value.args)
-        else:
-            raise Exception(
-                    'Evaluation not implemented for value {cls}.'.format(
-                       cls=type(value)
-                    )
-                  )
+        yield from (
+            self.yield_value(value) if value.is_decided()
+            else self.eval_expression(value.expr, value.env) if value.is_thunk()
+            else self.eval_flex_structure(value) if value.is_flex_structure()
+            else exception_with('Evaluation not implemented for value {cls}.'.format(cls=type(value)))
+        )
+
+    def eval_flex_structure(self, value):
+        assert value.symbol.is_instantiated()
+        yield from self.apply_many(value.symbol.representative(), value.args)
 
     def apply_many(self, value, vargs):
-        if len(vargs) == 0:
-            yield value
-        else:
-            for v in self.apply(value, vargs[0]):
+        yield from (
+            self.yield_value(value) if not vargs
+            else self.apply_many_aux(value, vargs)
+        )
+    
+    def apply_many_aux(self, value, vargs):
+        for v in self.apply(value, vargs[0]):
                 yield from self.apply_many(v, vargs[1:])
-
+            
     def apply(self, value, varg):
-        if value.is_thunk():
-            for v in self.eval_expression(value.expr, value.env):
+        yield from (
+            self.apply_expressions(value, varg) if value.is_thunk()
+            else self.apply_rigid(value, varg) if value.is_rigid_structure()
+            else self.apply_flex(value, varg) if value.is_flex_structure()
+            else self.apply_closure(value, varg) if value.is_closure()
+            else self.apply_primitive(value, varg) if value.is_primitive()
+            else exception_with('Application not implemented for {cls}.'.format(cls=type(value)))
+        )
+
+    def apply_expressions(self, value, varg):
+        for v in self.eval_expression(value.expr, value.env):
                 yield from self.apply(v, varg)
-        elif value.is_rigid_structure():
-            yield from self.apply_rigid(value, varg)
-        elif value.is_flex_structure():
-            yield from self.apply_flex(value, varg)
-        elif value.is_closure():
-            yield from self.apply_closure(value, varg)
-        elif value.is_primitive():
-            yield from self.apply_primitive(value, varg)
-        else:
-            raise Exception(
-                    'Application not implemented for {cls}.'.format(
-                       cls=type(value)
-                    )
-                  )
 
     def apply_rigid(self, value, varg):
         yield values.RigidStructure(value.constructor, value.args + [varg])
@@ -208,60 +197,52 @@ class Evaluator:
     def apply_primitive(self, value, varg):
         assert value.name in self._primitives
         vargs = value.args + [varg]
+
         if len(vargs) < self._primitives[value.name].arity:
             yield values.Primitive(value.name, vargs)
             return
-        if value.name == common.OP_SEQUENCE:
-            yield from self.primitive_sequence(*vargs)
-        elif value.name == common.OP_ALTERNATIVE:
-            yield from self.primitive_alternative(*vargs)
-        elif value.name == common.OP_UNIFY:
-            yield from self.primitive_unify(*vargs)
-        else:
-            raise Exception(
-                    'Primitive "{name}" not implemented.'.format(
-                       name=value.name
-                    )
-                  )
 
-    def primitive_sequence(self, val1, val2):
-        for v1 in self.eval_value(val1):
-            for v2 in self.eval_value(val2):
-                yield v2
+        yield from (
+            self.primitive_sequence(*vargs) if value.name == common.OP_SEQUENCE
+            else self.primitive_alternative(*vargs) if value.name == common.OP_ALTERNATIVE
+            else self.primitive_unify(*vargs) if value.name == common.OP_UNIFY
+            else exception_with('Primitive "{name}" not implemented.'.format(name=value.name))
+        )
+
+    def primitive_sequence(self, value1, value2):
+        for _ in self.eval_value(value1):
+            for v2 in self.eval_value(value2): yield v2
 
     def primitive_alternative(self, val1, val2):
-        for v in self.eval_value(val1):
-            yield v
-        for v in self.eval_value(val2):
-            yield v
+        for v in self.eval_value(val1): yield v
+        for v in self.eval_value(val2): yield v
 
     def primitive_unify(self, val1, val2):
         yield from self.unify([(val1, val2)])
 
     def unify(self, goals):
-        if len(goals) == 0:
-            yield values.unit()
-            return
+        if not goals: yield values.unit() ; return
 
-        (val1, val2) = goals[0]
+        (value1, value2) = goals[0]
         goals = goals[1:]
 
-        if not val1.is_decided():
-            for v1 in self.eval_value(val1):
-                yield from self.unify([(v1, val2)] + goals)
-            return
-        elif not val2.is_decided():
-            for v2 in self.eval_value(val2):
-                yield from self.unify([(val1, v2)] + goals)
+        if not value1.is_decided():
+            for v1 in self.eval_value(value1):
+                yield from self.unify([(v1, value2)] + goals)
             return
 
-        if val1.is_integer_constant() and val2.is_integer_constant():
-            if val1.value == val2.value:
-                yield from self.unify(goals)
-        elif val1.is_rigid_structure() and val2.is_rigid_structure():
-            if val1.constructor == val2.constructor and \
-               len(val1.args) == len(val2.args):
-                subgoals = list(zip(val1.args, val2.args))
+        elif not value2.is_decided():
+            for v2 in self.eval_value(value2):
+                yield from self.unify([(value1, v2)] + goals)
+            return
+
+        if value1.is_integer_constant() and value2.is_integer_constant():
+            if value1.value == value2.value: yield from self.unify(goals)
+
+        elif value1.is_rigid_structure() and value2.is_rigid_structure():
+            if value1.constructor == value2.constructor and \
+               len(value1.args) == len(value2.args):
+                subgoals = list(zip(value1.args, value2.args))
                 yield from self.unify(subgoals + goals)
 
         # Same head:
@@ -272,24 +253,24 @@ class Evaluator:
         #         and len(val1.args) == len(val2.args):
         #     subgoals = list(zip(val1.args, val2.args))
         #     yield from self.unify(subgoals + goals)
-        elif val1.is_flex_structure() and len(val1.args) == 0:
+        elif value1.is_flex_structure() and len(value1.args) == 0:
             # TODO: occurs check
-            assert not val1.symbol.is_instantiated() # decided
-            val1.symbol.instantiate(val2)
+            assert not value1.symbol.is_instantiated() # decided
+            value1.symbol.instantiate(value2)
             yield from self.unify(goals)
-            val1.symbol.uninstantiate()
-        elif val1.is_flex_structure() and len(val1.args) > 0:
+            value1.symbol.uninstantiate()
+        elif value1.is_flex_structure() and len(value1.args) > 0:
             # TODO: occurs check
-            assert not val1.symbol.is_instantiated() # decided
+            assert not value1.symbol.is_instantiated() # decided
             new_var = syntax.fresh_variable()
-            params = [syntax.fresh_variable() for arg in val1.args]
+            params = [syntax.fresh_variable() for arg in value1.args]
             term = syntax.lambda_many(
                      [p.name for p in params],
                      syntax.alternative(
                        syntax.sequence_many1(
                          [syntax.unify(p, a)
-                            for p, a in zip(params, val1.args)],
-                         val2 # body
+                            for p, a in zip(params, value1.args)],
+                         value2 # body
                        ),
                        syntax.application_many(new_var, params)
                      )
@@ -299,15 +280,15 @@ class Evaluator:
                        values.FlexStructure(
                          values.Metavar(prefix='F'),
                          []))
-            val1.symbol.instantiate(
+            value1.symbol.instantiate(
                 values.Thunk(
                     term,
                     env
                 )
             )
             yield from self.unify(goals)
-            val1.symbol.uninstantiate()
-        elif val2.is_flex_structure():
-            yield from self.unify([(val2, val1)] + goals)
+            value1.symbol.uninstantiate()
+        elif value2.is_flex_structure():
+            yield from self.unify([(value2, value1)] + goals)
         else:
             return # Otherwise we fail
